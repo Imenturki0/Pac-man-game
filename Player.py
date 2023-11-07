bc5a61d85d893b877b8307c75bb33551e04585de
imenturki <68286731+Imenturki0@users.noreply.github.com>
2023-11-07 22:50:29 +0300

    check position is corrected

--- Player.py
+++ Player.py
@@ -36,7 +36,7 @@ class Player:
 
         # Determine the current frame based on the counter
         current_frame = self.counter // 5
-        turns_allowed = self.check_pos(surface)
+        turns_allowed = self.check_positions()
         # Draw the player based on the current direction
 
         if self.direction == 0:
@@ -52,77 +52,107 @@ class Player:
             self.last_valid_image = pygame.transform.rotate(self.player_images[current_frame], 270)
             surface.blit(self.last_valid_image, (self.position_x, self.position_y))
 
-    def check_pos(self, screen):
-
-        turns = [False] * 4
-
-        # Calculate the pixel coordinates for the edges of the object
-        left_edge_x = self.position_x
-        right_edge_x = self.position_x + self.num_height - 7
-        top_edge_y = self.position_y + 5
-        bottom_edge_y = self.position_y + self.num_width - 7
-        pygame.draw.circle(screen, (255, 255, 255), (left_edge_x, top_edge_y), 2)
-        pygame.draw.circle(screen, (255, 255, 255), (left_edge_x, bottom_edge_y), 2)
-        pygame.draw.circle(screen, (255, 255, 255), (right_edge_x, top_edge_y), 2)
-        pygame.draw.circle(screen, (255, 255, 255), (right_edge_x, bottom_edge_y), 2)
-        # Calculate the grid indices from the pixel coordinates
-        left_edge_index = left_edge_x // self.num_width
-        right_edge_index = right_edge_x // self.num_width
-        top_edge_index = top_edge_y // self.num_height
-        bottom_edge_index = bottom_edge_y // self.num_height
-
-        # Now perform the checks using indices as necessary
-
-        # Check Right
-        if boards1[self.center_y // self.num_height][((self.center_x + (self.num_width // 2)) // self.num_width)] != 4:
-            if boards1[top_edge_index][right_edge_index] == 4 or boards1[bottom_edge_index][
-                right_edge_index] == 4:  # Ensure within right boundary
-                turns[0] = False
-            else:
-                turns[0] = True
-
-        # Check Left
-        if boards1[self.center_y // self.num_height][(self.center_x - (self.num_width // 2)) // self.num_width] != 4:
-            if boards1[top_edge_index][left_edge_index] == 4 or boards1[bottom_edge_index][
-                left_edge_index] == 4:  # Ensure within right boundary
-                turns[1] = False
-            else:
-                turns[1] = True
-
-        # Check Up
-        if boards1[(self.center_y - (self.num_height // 2)) // self.num_height][(self.center_x // self.num_width)] != 4:
-
-            if boards1[top_edge_index][left_edge_index] == 4 or boards1[top_edge_index][right_edge_index] == 4:
-                turns[2] = False
-
-            else:
-                turns[2] = True
-
-        # Check Down
-        if boards1[(self.center_y + (self.num_height // 2)) // self.num_height][(self.center_x // self.num_width)] != 4:
-            # Ensure within right boundary
-            if boards1[bottom_edge_index][left_edge_index] == 4 or boards1[bottom_edge_index][right_edge_index] == 4:
-                turns[3] = False
-
-            else:
-                turns[3] = True
-        # print(turns)
-        return turns
+    # def check_pos(self, screen):
+    #
+    #     turns = [False] * 4
+    #
+    #     # Calculate the pixel coordinates for the edges of the object
+    #     left_edge_x = self.position_x
+    #     right_edge_x = self.position_x + self.num_height - 7
+    #     top_edge_y = self.position_y + 5
+    #     bottom_edge_y = self.position_y + self.num_width - 7
+    #     pygame.draw.circle(screen, (255, 255, 255), (left_edge_x, top_edge_y), 2)
+    #     pygame.draw.circle(screen, (255, 255, 255), (left_edge_x, bottom_edge_y), 2)
+    #     pygame.draw.circle(screen, (255, 255, 255), (right_edge_x, top_edge_y), 2)
+    #     pygame.draw.circle(screen, (255, 255, 255), (right_edge_x, bottom_edge_y), 2)
+    #     # Calculate the grid indices from the pixel coordinates
+    #     left_edge_index = left_edge_x // self.num_width
+    #     right_edge_index = right_edge_x // self.num_width
+    #     top_edge_index = top_edge_y // self.num_height
+    #     bottom_edge_index = bottom_edge_y // self.num_height
+    #
+    #     # Now perform the checks using indices as necessary
+    #
+    #     # Check Right
+    #     if boards1[self.center_y // self.num_height][((self.center_x + (self.num_width // 2)) // self.num_width)] != 4:
+    #         if boards1[top_edge_index][right_edge_index] == 4 or boards1[bottom_edge_index][
+    #             right_edge_index] == 4:  # Ensure within right boundary
+    #             turns[0] = False
+    #         else:
+    #             turns[0] = True
+    #
+    #     # Check Left
+    #     if boards1[self.center_y // self.num_height][(self.center_x - (self.num_width // 2)) // self.num_width] != 4:
+    #         if boards1[top_edge_index][left_edge_index] == 4 or boards1[bottom_edge_index][
+    #             left_edge_index] == 4:  # Ensure within right boundary
+    #             turns[1] = False
+    #         else:
+    #             turns[1] = True
+    #
+    #     # Check Up
+    #
+    #     if boards1[(self.center_y - (self.num_height // 2)) // self.num_height][(self.center_x // self.num_width)] != 4:
+    #
+    #         if boards1[top_edge_index][left_edge_index] == 4 or boards1[top_edge_index][right_edge_index] == 4:
+    #             turns[2] = False
+    #
+    #         else:
+    #             turns[2] = True
+    #
+    #     # Check Down
+    #     if boards1[(self.center_y + (self.num_height // 2)) // self.num_height][(self.center_x // self.num_width)] != 4:
+    #         # Ensure within right boundary
+    #         if boards1[bottom_edge_index][left_edge_index] == 4 or boards1[bottom_edge_index][right_edge_index] == 4:
+    #             turns[3] = False
+    #
+    #         else:
+    #             turns[3] = True
+    #
+    #     if self.direction == 2 or self.direction == 3:
+    #
+    #         if 16 <= self.center_x % self.num_width <= 22:
+    #             if boards1[(self.center_y + (self.num_height // 2)) // self.num_height][
+    #                 self.center_x // self.num_width] != 4:
+    #                 turns[3] = True
+    #             if boards1[(self.center_y - (self.num_height // 2)) // self.num_height][
+    #                 self.center_x // self.num_width] != 4:
+    #                 turns[2] = True
+    #         if 16 <= self.center_y % self.num_height <= 22:
+    #             if boards1[self.center_y // self.num_height][(self.center_x - self.num_width) // self.num_width] != 4:
+    #                 turns[1] = True
+    #             if boards1[self.center_y // self.num_height][(self.center_x + self.num_width) // self.num_width] != 4:
+    #                 turns[0] = True
+    #
+    #     if self.direction == 0 or self.direction == 1:
+    #         if 16 <= self.center_x % self.num_width <= 22:
+    #             if boards1[(self.center_y + self.num_height) // self.num_height][self.center_x // self.num_width] != 4:
+    #                 turns[3] = True
+    #             if boards1[(self.center_y - self.num_height) // self.num_height][self.center_x // self.num_width] != 4:
+    #                 turns[2] = True
+    #         if 12 <= self.center_y % self.num_height <= 18:
+    #             if boards1[self.center_y // self.num_height][(self.center_x - (self.num_width // 2)) // self.num_width] != 4:
+    #                 turns[1] = True
+    #             if boards1[self.center_y // self.num_height][(self.center_x + (self.num_width // 2)) // self.num_width] != 4:
+    #                 turns[0] = True
+    #     # print(turns)
+    #     return turns
 
     def check_positions(self):
         turns = [False] * 4
-        num = 16
+
+         #print(self.center_y // self.num_height,(self.center_x - num) // self.num_width)
+
         if self.direction == 0:
-            if boards1[self.center_y // self.num_height][(self.center_x - num) // self.num_width] != 4:
+            if boards1[self.center_y // self.num_height][(self.center_x - (self.num_width // 2)) // self.num_width] != 4:
                 turns[1] = True
         if self.direction == 1:
-            if boards1[self.center_y // self.num_height][(self.center_x + num) // self.num_width] != 4:
+            if boards1[self.center_y // self.num_height][(self.center_x + (self.num_width // 2)) // self.num_width] != 4:
                 turns[0] = True
         if self.direction == 2:
-            if boards1[self.center_y + (self.num_height // 2) // self.num_height][(self.center_x) // self.num_width] != 4:
+            if boards1[(self.center_y + (self.num_height // 2)) // self.num_height][(self.center_x) // self.num_width] != 4:
                 turns[3] = True
         if self.direction == 3:
-            if boards1[self.center_y - (self.num_height // 2) // self.num_height][(self.center_x) // self.num_width] != 4:
+            if boards1[(self.center_y - (self.num_height // 2)) // self.num_height][(self.center_x) // self.num_width] != 4:
                 turns[2] = True
 
         if self.direction == 2 or self.direction == 3:
@@ -144,10 +174,10 @@ class Player:
                     turns[3] = True
                 if boards1[(self.center_y - self.num_height) // self.num_height][self.center_x // self.num_width] != 4:
                     turns[2] = True
-            if 12 <= self.center_y % self.num_height <= 18:
-                if boards1[self.center_y // self.num_height][(self.center_x - num) // self.num_width] != 4:
+            if 16 <= self.center_y % self.num_height <= 22:
+                if boards1[self.center_y // self.num_height][(self.center_x - (self.num_width // 2)) // self.num_width] != 4:
                     turns[1] = True
-                if boards1[self.center_y // self.num_height][(self.center_x + num) // self.num_width] != 4:
+                if boards1[self.center_y // self.num_height][(self.center_x + (self.num_width // 2)) // self.num_width] != 4:
                     turns[0] = True
         return turns
 
